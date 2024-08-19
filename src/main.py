a3554cedb3d7128bc57fffe187577539c98b88c3
Shinzatoo <felipeshinzatojr@gmail.com>
2024-08-19 10:55:38 -0300

    Simulacao de dois carros

--- src/main.py
+++ src/main.py
@@ -1,6 +1,5 @@
 from frota import *
-
-def simular_carro(carro: Carro):
+def simular_carro(carro : Carro):
     print('1- Ligar motor')
     print('2- Desligar motor')
     print('3- Acelerar')
@@ -18,41 +17,43 @@ def simular_carro(carro: Carro):
         t = float(input("Informe o tempo: "))
         carro.acelerar(v, t)
 
+    print('Infos atuais do carro')
+    print(carro1)
+    print(carro2)
+
 if __name__ == "__main__":
     print('Cadastre um carro1')
     nm_modelo = input('Digite o modelo: ')
     nm_marca = input('Digite a marca: ')
     nm_cor = input('Digite a cor: ')
+    litros = float(input("digite o nivel do tanque?: "))
+    cm_medio = float(input("digite quanto o carro gasta: "))
 
-    carro1 = Carro(nm_modelo, nm_marca, nm_cor, 0, False)
+    carro1 = Carro(nm_modelo, nm_marca, nm_cor,0, False,litros,cm_medio)
 
     print('Cadastre um carro2')
     nm_modelo = input('Digite o modelo: ')
     nm_marca = input('Digite a marca: ')
     nm_cor = input('Digite a cor: ')
+    litros = float(input("digite o nivel do tanque?: "))
+    cm_medio = float(input("digite quanto o carro gasta?: "))
 
-    carro2 = Carro(nm_modelo, nm_marca, nm_cor, 0, False)
+    carro2 = Carro(nm_modelo, nm_marca, nm_cor, 0, False,litros,cm_medio)
 
     '''
-    Controlando o carro até ele atingir 10000 Km
+    Controlando o 2 carros até ele atingir 300 Km
     '''
-    while carro1.odometro < 300 and carro2.odometro < 300:
+    while carro1.odometro < 300 and carro2.odometro < 300 and (carro1.tanque >0 or carro2.tanque >0):
         try:
             op_carro = 0
-            while op_carro not in (1, 2):
-                op_carro = int(input("Qual carro simular 1 ou 2?"))
+            while op_carro not in (1,2):
+                op_carro = int(input("qual carro simular 1 ou 2?: "))
 
             if op_carro == 1:
                 simular_carro(carro1)
             else:
                 simular_carro(carro2)
         except Exception as e:
-            print("Erro!")
+            print("ERRO!")
             print(e)
-
-
-    carro1.desligar()
-    carro2.desligar()
-    print(carro1)
-    print(carro2)
 
